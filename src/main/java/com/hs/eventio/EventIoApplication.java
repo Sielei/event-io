58612a29a22e34db574c89caacbe71d4ff948f44
Sielei Herman <hsielei@gmail.com>
2024-04-26 15:46:57 +0300

    first commit

---
+++ src/main/java/com/hs/eventio/EventIoApplication.java
@@ -0,0 +1,13 @@
+package com.hs.eventio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventIoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventIoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hs/eventio/EventIoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hs.eventio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventIoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
