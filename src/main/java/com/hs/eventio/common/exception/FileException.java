2a7c88f37f30e903f9184b23866659ed86a99dd4
Sielei Herman <hsielei@gmail.com>
2024-04-28 19:49:37 +0300

    chore: set up file upload class that will be reused for photo uploads. This will handle uploads of both user photos and event photos.

---
+++ src/main/java/com/hs/eventio/common/exception/FileException.java
@@ -0,0 +1,7 @@
+package com.hs.eventio.common.exception;
+
+public class FileException extends RuntimeException{
+    public FileException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/hs/eventio/common/service/FileUploadService.java
@@ -0,0 +1,57 @@
+package com.hs.eventio.common.service;
+
+import com.hs.eventio.common.exception.FileException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Objects;
+
+@Service
+public class FileUploadService {
+    private final Path fileUploadLocation;
+    private static final Logger log = LoggerFactory.getLogger(FileUploadService.class);
+
+    public FileUploadService() {
+        this.fileUploadLocation = Paths.get("/uploads").toAbsolutePath().normalize();
+        try {
+            Files.createDirectories(this.fileUploadLocation);
+        }
+        catch (Exception exception){
+            log.error("Failed to create upload directory!",exception);
+        }
+    }
+
+    public String uploadFile(MultipartFile multipartFile) {
+        var fileName = StringUtils.cleanPath(Objects.requireNonNull(multipartFile.getOriginalFilename()));
+        try {
+            var targetLocation = this.fileUploadLocation.resolve(fileName);
+            Files.copy(multipartFile.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+        }
+        catch (IOException exception){
+            log.error("Could not upload file!", exception);
+        }
+        return fileName;
+    }
+
+    public Resource loadFileAsResource(String fileName) throws MalformedURLException {
+        var filePath = this.fileUploadLocation.resolve(fileName).normalize();
+        var resource = new UrlResource(filePath.toUri());
+        if (resource.exists()){
+            return resource;
+        }
+        else {
+            throw new FileException("File not found: " + fileName);
+        }
+    }
+}
