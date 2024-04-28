77b366fb0f7bc9420fa79ae3c1ac078210e427d7
Sielei Herman <hsielei@gmail.com>
2024-04-28 19:47:53 +0300

    chore: set up spring security to use JWT filter for authentication. Allow anonymous access for auth routes and swagger ui

---
+++ src/main/java/com/hs/eventio/auth/CustomUserDetailService.java
@@ -0,0 +1,37 @@
+package com.hs.eventio.auth;
+
+import com.hs.eventio.user.UserService;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+    private final UserService userService;
+
+    public CustomUserDetailService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var userDTO = userService.findUserByUsername(username);
+        return new User(userDTO.name(), userDTO.password(), getAuthorities(userDTO));
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(
+            AuthDTO.FindUserResponse userDTO) {
+        var grantedAuthorities = new ArrayList<GrantedAuthority>();
+        for (var role: userDTO.roles()){
+            grantedAuthorities.add(new SimpleGrantedAuthority(role));
+        }
+        return grantedAuthorities;
+    }
+}
---
+++ src/main/java/com/hs/eventio/auth/JwtUtil.java
@@ -0,0 +1,61 @@
+package com.hs.eventio.auth;
+
+import com.hs.eventio.user.UserService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.time.Instant;
+import java.time.ZonedDateTime;
+import java.util.Date;
+import java.util.UUID;
+
+@Component
+class JwtUtil {
+    private final UserService userService;
+
+    private static final String SECRET = "pYXQiOjEMsnVz5AYXJvbXMNjc2OTgyNTUzfQ";
+    private static final SecretKey SECRET_KEY = Keys.hmacShaKeyFor(SECRET.getBytes());
+
+    public JwtUtil(UserService userService) {
+        this.userService = userService;
+    }
+
+    public String generateJWTToken(AuthDTO.FindUserResponse userDto){
+        return Jwts.builder()
+                .issuedAt(Date.from(ZonedDateTime.now().toInstant()))
+                .issuer("Lunar HR")
+                .claim("userId", userDto.id())
+                .claim("username", userDto.email())
+//                .setExpiration(Date.from(ZonedDateTime.now().plusHours(1).toInstant()))
+                .expiration(Date.from(ZonedDateTime.now().plusDays(30).toInstant()))
+                .signWith(SECRET_KEY)
+                .compact();
+    }
+
+    public boolean validateJWTToken(String token){
+        return getUsername(token) != null && isExpired(token);
+    }
+
+    private boolean isExpired(String token) {
+        Claims claims = getClaims(token);
+        return claims.getExpiration().after(new Date(System.currentTimeMillis()));
+    }
+
+    public String getUsername(String token) {
+        Claims claims = getClaims(token);
+        return claims.get("username").toString();
+    }
+    private Claims getClaims(String token) {
+        return Jwts.parser().verifyWith(SECRET_KEY).build().parseSignedClaims(token).getPayload();
+    }
+    public UUID getUserIdFromJWTToken(String token){
+        return UUID.fromString(getClaims(token).get("userId").toString());
+    }
+    public Instant getTokenExpiry(String jwtToken) {
+        Claims claims = getClaims(jwtToken);
+        return claims.getExpiration().toInstant();
+    }
+}
