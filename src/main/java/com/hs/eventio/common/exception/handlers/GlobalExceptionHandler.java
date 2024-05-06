fbf81548c78c9eedc60086c9d5ca2690433f581d
Sielei Herman <hsielei@gmail.com>
2024-05-07 01:58:38 +0300

    handle bad credentials exception

--- src/main/java/com/hs/eventio/common/exception/handlers/GlobalExceptionHandler.java
+++ src/main/java/com/hs/eventio/common/exception/handlers/GlobalExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.hs.eventio.common.exception.handlers;
 
 import com.hs.eventio.common.exception.ResourceNotFoundException;
+import com.hs.eventio.common.exception.UserAuthenticationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -21,6 +23,20 @@ public class GlobalExceptionHandler {
                 .build().toUri();
         var problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
         problemDetail.setTitle("Resource Not Found");
+        problemDetail.setType(location);
+        problemDetail.setProperty("timestamp", DateTimeFormatter
+                .ofPattern("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH)
+                .format(LocalDateTime.now()));
+        return problemDetail;
+    }
+
+    @ExceptionHandler(value = {BadCredentialsException.class, UserAuthenticationException.class})
+    ProblemDetail handleBadCredentialsException(RuntimeException e){
+        var location = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/api/v1/errors/authentication-error")
+                .build().toUri();
+        var problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED, e.getMessage());
+        problemDetail.setTitle("Authentication Error");
         problemDetail.setType(location);
         problemDetail.setProperty("timestamp", DateTimeFormatter
                 .ofPattern("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH)
