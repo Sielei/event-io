6e88f8ade1f5220ac4779fbe05202813077d6cc5
Sielei Herman <hsielei@gmail.com>
2024-05-10 19:04:00 +0300

    handle MethodArgumentNotValidException

--- src/main/java/com/hs/eventio/common/exception/handlers/GlobalExceptionHandler.java
+++ src/main/java/com/hs/eventio/common/exception/handlers/GlobalExceptionHandler.java
@@ -5,13 +5,16 @@ import com.hs.eventio.common.exception.UserAuthenticationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Locale;
+import java.util.*;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -41,6 +44,29 @@ public class GlobalExceptionHandler {
         problemDetail.setProperty("timestamp", DateTimeFormatter
                 .ofPattern("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH)
                 .format(LocalDateTime.now()));
+        return problemDetail;
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    ProblemDetail handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        var problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, e.getMessage());
+        problemDetail.setTitle("Validation Error");
+        problemDetail.setType(URI.create("ValidationError"));
+        problemDetail.setProperty("timestamp", DateTimeFormatter
+                .ofPattern("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH)
+                .format(LocalDateTime.now()));
+        var errors = new HashMap<String, List<String>>();
+        e.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            if (!errors.containsKey(fieldName)) {
+                errors.put(fieldName,
+                        new ArrayList<>(Collections.singletonList(error.getDefaultMessage())));
+            }
+            else {
+                errors.get(fieldName).add(error.getDefaultMessage());
+            }
+        });
+        problemDetail.setProperty("errors", errors);
         return problemDetail;
     }
 }
